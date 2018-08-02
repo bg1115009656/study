eaf224d21d23a911418e2b430910a76ac1228930
baigang <baigang1995@gmail.com>
2018-08-02 09:17:33 +0800

    添加security和jwt整合案例

---
+++ src/main/java/com/gbai/dao/UserMapper.java
@@ -0,0 +1,16 @@
+package com.gbai.dao;
+
+import com.gbai.entity.User;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author codeGenerate
+ * @since 2018-08-01
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
---
+++ src/main/java/com/gbai/service/UserService.java
@@ -0,0 +1,16 @@
+package com.gbai.service;
+
+import com.gbai.entity.User;
+import com.baomidou.mybatisplus.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author codeGenerate
+ * @since 2018-08-01
+ */
+public interface UserService extends IService<User> {
+
+}
