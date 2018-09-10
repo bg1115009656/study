14e5b5e3635658a1a614798ede15dcf0aa358697
baigang <baigang1995@gmail.com>
2018-09-10 10:43:23 +0800

    mybatisplus修改

--- mybatisplus/src/test/java/com/gbai/MybatisplustestApplicationTests.java
+++ mybatisplus/src/test/java/com/gbai/MybatisplustestApplicationTests.java
@@ -1,22 +1,27 @@
 package com.gbai;
 
+import com.gbai.dao.UserMapper;
 import com.gbai.entity.User;
 import com.gbai.service.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MybatisplustestApplicationTests {
 
+    @Autowired
+    UserMapper userMapper;
+
     @Test
     public void contextLoads() {
-        User user = new User();
-        List<User> users = user.selectAll();
+        List<Map<String, Object>> users = userMapper.getAllUser();
         System.out.println(users.toString());
     }
 
