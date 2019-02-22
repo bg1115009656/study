e230317a4ed903792b0db151ccb818fa4c1eec02
baigang <baigang1995@gmail.com>
2019-02-22 09:34:06 +0800

    算法测试

--- quartz/src/main/java/com/gbai/quartz/config/QuartzConfig.java
+++ quartz/src/main/java/com/gbai/quartz/config/QuartzConfig.java
@@ -9,25 +9,25 @@ import org.springframework.context.annotation.Configuration;
 
 /**
  * @program: study
- * @description: 定时任务配置
+ * @description: 定时任务配置springboot2.0支持
  * @author: gbai
  * @create: 2019-01-03 11:56
  **/
-//@Configuration
-//public class QuartzConfig {
-//
-//    @Bean
-//    public JobDetail uploadTaskDetail() {
-//        return JobBuilder.newJob(QuartzTask.class).withIdentity("quartzTask").storeDurably().build();
-//    }
-//
-//    @Bean
-//    public Trigger uploadTaskTrigger() {
-//        CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule("*/5 * * * * ?");
-//        return TriggerBuilder.newTrigger().forJob(uploadTaskDetail())
-//                .withIdentity("quartzTask")
-//                .withSchedule(scheduleBuilder)
-//                .build();
-//    }
-//
-//}
+@Configuration
+public class QuartzConfig {
+
+    @Bean
+    public JobDetail uploadTaskDetail() {
+        return JobBuilder.newJob(QuartzTask.class).withIdentity("quartzTask").storeDurably().build();
+    }
+
+    @Bean
+    public Trigger uploadTaskTrigger() {
+        CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule("*/5 * * * * ?");
+        return TriggerBuilder.newTrigger().forJob(uploadTaskDetail())
+                .withIdentity("quartzTask")
+                .withSchedule(scheduleBuilder)
+                .build();
+    }
+
+}
